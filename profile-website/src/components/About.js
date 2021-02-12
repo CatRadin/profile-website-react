b0b7b9f62a99e2c71d05070130cbbe5be2becb4b
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-12 16:37:24 -0600

    animated background

--- profile-website/src/components/About.js
+++ profile-website/src/components/About.js
@@ -4,7 +4,9 @@ function About() {
 
 return(
     <div class="grid-container">
-    <div class="image-containter"></div>
+    <div class="image-container">
+        <img className="me" src='https://i.ibb.co/jv4g6zK/blackandwhiteme.png' />
+    </div>
     <div class="about"></div>
     <div class="skills"></div>
   </div>
