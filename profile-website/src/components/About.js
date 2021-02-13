5f00e6b1b40df623439e0708ca4003029bd37420
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-12 23:40:03 -0600

    added fade in annimations

--- profile-website/src/components/About.js
+++ profile-website/src/components/About.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import HTML from '../images/html.png'
+import React, { useEffect } from 'react'
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 function About() {
+    useEffect(() => {
+        Aos.init({ duration: 1500 });
+    },[])
 
 return(
     <div class="grid-container">
-    <div class="image-container">
+    <div data-aos="fade-right" class="image-container">
         <img className="me" src='https://i.ibb.co/jv4g6zK/blackandwhiteme.png' />
         <img className='myName' src="https://i.ibb.co/5xLntwB/Name-Lighter.png" />
         <p>Full Stack Developer</p>
         <img className="contact-stuff" src="https://i.ibb.co/DVdmnRY/Contact-Details.png" />
     </div>
-    <div class="about">
-        <h1>About Me: </h1>
+    <div data-aos="fade-down"  class="about">
+        <h1>About <span className='spanMe'>Me:</span> </h1>
         <p>Cupcake ipsum dolor sit amet I love. Bear claw carrot cake cake gingerbread pastry bear claw marshmallow. Marzipan brownie cake I love soufflé pudding lollipop topping. I love tiramisu bonbon. Topping marzipan sweet roll dessert. I love I love candy I love jujubes toffee jujubes chocolate bar bear claw. Tiramisu lemon drops caramels tootsie roll pastry sweet roll halvah donut. Brownie soufflé toffee brownie. I love pudding I love jelly tiramisu gummies marshmallow pastry cotton candy. Halvah chocolate I love jelly beans. Macaroon cake donut cheesecake dragée muffin bonbon icing. I love soufflé lollipop brownie marshmallow muffin jelly beans sweet. Danish toffee sweet danish sesame snaps jelly beans caramels.</p>
     </div>
-    <div class="skills">
+    <div data-aos="fade-up" class="skills">
         <h1>Skills: </h1>
         <ul>
             {/* <div className='skillz'>
