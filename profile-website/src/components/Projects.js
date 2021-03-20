4918f756ce6503d0b91dfa511e7df78857488334
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-03-20 14:42:10 -0500

    working on projects

--- profile-website/src/components/Projects.js
+++ profile-website/src/components/Projects.js
@@ -4,9 +4,13 @@ import Aos from "aos"
 function Projects() {
 
 return(
-    <div class="projects">
+  <>
   <h1 data-aos="fade-right" className="project-title">Projects</h1>
+  <div class="projects">
+  <div className="project-left">dfd</div>
+  <div className="project-right">fdsfds</div>
   </div>
+  </>
 )
 }
 export default Projects
