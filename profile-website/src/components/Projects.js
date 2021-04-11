5fd195e1a75d68f918550681eb2b529a268b3693
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-04-11 18:24:42 -0500

    fished up with projects section

--- profile-website/src/components/Projects.js
+++ profile-website/src/components/Projects.js
@@ -1,9 +1,13 @@
-import React from 'react'
+
+import React, { useEffect } from 'react'
 import Aos from "aos"
+import "aos/dist/aos.css"
 
 function Projects() {
 
-  
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+},[])
 
 
 
@@ -17,13 +21,15 @@ return(
     <div clssName='title-container'>
   <h1 data-aos="fade-right" className="project-title">Projects</h1>
   </div>
-  <div className="project-left">
+  <div data-aos="fade-left" className="project-left">
     <div className='p-left-image'>
     <img className="project-img" src="https://i.ibb.co/ZLFk69C/randompokemon.png"></img>
     </div>
     <div className ='p-left-content'>
       <h1 className="left-title">Random Pokemon</h1>
-      <p> | Redux |  JavaScript | CSS | HTML | JavaScript | <br /> A school project learning Redux. I used the pokemon API to grab a random pokemon then fetched the returned URL to find /its name and type! </p>
+       <p className='languages-used'> | Redux |  React | CSS | HTML | JavaScript | API | 
+       </p>
+       <p className='project-about-p'> I used the pokemon API to grab a random pokemon then fetched the returned URL to find it's name and type! I then displayed them for the user. </p>
       <div className='left-buttons'>
       <a className='left-links' href="https://github.com/CatRadin/React-Redux-App/tree/catherine-radin" 
         target="_blank">Github</a>
@@ -32,25 +38,28 @@ return(
       </div>
     </div>
   </div>
+<div className='divider'>|</div>
+<div className='divider'>|</div>
+<div className='divider'>|</div>
+<div className='divider'>|</div>
 
-  <div className="project-right">
-  <div className='p-right-image'>
-    <img className="project-img" src="https://i.ibb.co/ZLFk69C/randompokemon.png"></img>
-    </div>
+<div data-aos="fade-right" className="project-right">
     <div className ='p-left-content'>
-      <h1 className="left-title">Random Pokemon</h1>
-      <p>| Redux |  JavaScript | CSS | HTML | JavaScript <br /> 
-      A school project learning Redux. I used the pokemon API to grab a random pokemon then fetched the returned URL to find /its name and type! </p>
+      <h1 className="left-title nasa-title">Nasa Photo of the Day</h1>
+       <p className='languages-used'> | React |  JavaScript | CSS | JavaScript | API | 
+       </p>
+       <p className='project-about-p'> I used the pokemon API to grab a random pokemon then fetched the returned URL to find it's name and type! I then displayed them for the user. </p>
       <div className='left-buttons'>
-      <a className='left-links' href="https://github.com/CatRadin/React-Redux-App/tree/catherine-radin" 
+      <a className='left-links' href="https://github.com/CatRadin/space-picture-of-day" 
         target="_blank">Github</a>
-        <a className='left-links' href="https://random-pokemon.netlify.app/" 
+        <a className='left-links' href="https://nasa-photo-a-day-catherine.netlify.app/" 
         target="_blank">Demo</a>
       </div>
     </div>
-
-
-  </div>
+    <div className='p-left-image'>
+    <img className="project-img nasa-img" src="https://i.ibb.co/C8HtqGk/Nasa-Photo-Of-The-Day.png"></img>
+    </div>
+</div>
   </div>
   </>
 
