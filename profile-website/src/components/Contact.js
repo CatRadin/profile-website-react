fb29008ec2db446b3786757583f74da757dba656
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-07-13 16:09:59 -0500

    Improved Projects

--- profile-website/src/components/Contact.js
+++ profile-website/src/components/Contact.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 function Contact() {
 
+  
+
 return(
     <div class="contact-container" id="Contact">
       
--- profile-website/src/components/Projects.js
+++ profile-website/src/components/Projects.js
@@ -16,23 +16,38 @@ function Projects() {
 
 return(
   <>
-  
+  <div class="text-container">
+  <span>P</span>
+  <span>r</span>
+  <span>o</span>
+  <span>j</span>
+  <span>e</span>
+  <span>c</span>
+  <span>t</span>
+  <span>s</span>
+</div>
   <div class="projects" id="projects">
     <div clssName='title-container'>
-  <h1 data-aos="fade-right" className="project-title">Projects</h1>
+
+    
+
+  {/* <h1 data-aos="fade-right" className="project-title">Projects</h1> */}
   </div>
 
-  <div data-aos="fade-right" className="project-right HRF-right">
-    <div className ='p-left-content'>
+  <div  className="project-right HRF-right">
+    <div data-aos="fade-right" className ='p-left-content'>
       <h1 className="left-title nasa-title">Human Rights First</h1>
+      <img  className='pink-bar-projects' src="https://i.ibb.co/rtMV6Qx/Pink-bar.png"></img>
        <p className='languages-used'> JavaScript | React | Less | Ant Design | Node.js | PostgreSQL | Figma 
        </p>
        <p className='project-about-p'>I collaborated remotely with a team of Web Developers, UX/UI Lead Designers and Data Scientists to add new features.</p>
+      
       <div className='left-buttons'>
+
       <a className='left-links' href="https://github.com/CatRadin/human-rights-first-asylum-fe-a" 
         target="_blank">Github</a> 
         <a className='left-links' href="https://a.humanrightsfirstasylum.dev/login" 
-        target="_blank">Site</a>
+        target="_blank">Demo</a>
       </div>
     </div>
     <div className='p-left-image'>
@@ -41,12 +56,13 @@ return(
 </div>
 
 
-  <div data-aos="fade-left" className="project-left">
-    <div className='p-left-image'>
+  <div className="project-left">
+    <div data-aos="fade-left" className='p-left-image'>
     <img className="project-img" src="https://i.ibb.co/ZLFk69C/randompokemon.png"></img>
     </div>
     <div className ='p-left-content'>
       <h1 className="left-title">Random Pokemon</h1>
+      <img  className='pink-bar-projects' src="https://i.ibb.co/rtMV6Qx/Pink-bar.png"></img>
        <p className='languages-used'> | Redux | React | CSS | HTML | JavaScript | API | 
        </p>
        <p className='project-about-p'> I used the pokemon API to grab a random pokemon then fetched the returned URL to find it's name and type! I then displayed them for the user. </p>
@@ -59,14 +75,10 @@ return(
     </div>
   </div>
 
-{/* <div className='divider'></div>
-<div className='divider'></div>
-<div className='divider'></div>
-<div className='divider'></div> */}
-
-<div data-aos="fade-right" className="project-right">
-    <div className ='p-left-content'>
+<div className="project-right">
+    <div data-aos="fade-right" className ='p-left-content'>
       <h1 className="left-title nasa-title">Nasa Photo of the Day</h1>
+      <img className='pink-bar-projects' src="https://i.ibb.co/rtMV6Qx/Pink-bar.png"></img>
        <p className='languages-used'> | React | JavaScript | CSS | JavaScript | API | 
        </p>
        <p className='project-about-p'> I used the nasa API to grab their photo of the day. Along with the iformation about the image. </p>
